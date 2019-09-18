59c72da7e115e0febb8dedabccf48bcb407d107d
riojack <ayjay122@gmail.com>
2019-09-18 17:07:03 -0500

    start screen manager, which right now just prints screen names

---
+++ Zoo/Ui/ScreenManager.cs
@@ -0,0 +1,24 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Zoo.Ui
+{
+    public class ScreenManager
+    {
+        public ICollection<IScreen> Screens { get; set; }
+
+        public TextWriter Out { get; set; }
+        public TextReader In { get; set; }
+
+        public async Task Go()
+        {
+            foreach (var screen in Screens)
+            {
+                Out.WriteLine(screen.Name);
+            }
+        }
+    }
+}
---
+++ ZooTest/Ui/ScreenManagerTest.cs
@@ -0,0 +1,44 @@
+using System.Collections.Generic;
+using System.IO;
+using Moq;
+using Xunit;
+using Zoo.Ui;
+
+namespace ZooTest.Ui
+{
+    public class ScreenManagerTest
+    {
+        private const string ScreenName = "Test Screen 1";
+        private Mock<IScreen> MockScreen { get; set; }
+
+        private Mock<TextWriter> MockTextWriter { get; set; }
+
+        private Mock<TextReader> MockTextReader { get; set; }
+
+        private ScreenManager ScreenManager { get; set; }
+
+        public ScreenManagerTest()
+        {
+            MockScreen = new Mock<IScreen>();
+            MockScreen.Setup(x => x.Name).Returns(ScreenName);
+
+            MockTextWriter = new Mock<TextWriter>();
+            MockTextReader = new Mock<TextReader>();
+
+            ScreenManager = new ScreenManager
+            {
+                Screens = new List<IScreen> {MockScreen.Object},
+                Out = MockTextWriter.Object,
+                In = MockTextReader.Object
+            };
+        }
+
+        [Fact]
+        public void ShouldRenderAllScreenNamesToOutput()
+        {
+            ScreenManager.Go();
+
+            MockTextWriter.Verify(x => x.WriteLine(ScreenName), Times.Once);
+        }
+    }
+}
