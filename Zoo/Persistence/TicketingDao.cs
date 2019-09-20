fe1a23573c668bad5b0466de8b7621f2cb828057
riojack <ayjay122@gmail.com>
2019-09-20 09:26:36 -0500

    saved ticket can be fetched again

--- Zoo/Persistence/TicketingDao.cs
+++ Zoo/Persistence/TicketingDao.cs
@@ -1,19 +1,27 @@
 using System;
+using System.Collections.Generic;
 using Zoo.Domain;
 
 namespace Zoo.Persistence
 {
     public class TicketingDao
     {
+        private IDictionary<string, Ticket> Tickets { get; set; } = new Dictionary<string, Ticket>();
+
         public void Save(Ticket ticket)
         {
             var id = Guid.NewGuid().ToString();
 
             ticket.Id = id;
+
+            Tickets.Add(ticket.Id, ticket);
         }
 
-        public void Find(string ticketId)
+        public Ticket Find(string ticketId)
         {
+            Tickets.TryGetValue(ticketId, out var ticket);
+
+            return ticket;
         }
 
         public void Remove(string ticketId)
--- ZooTest/Persistence/TicketingDaoTest.cs
+++ ZooTest/Persistence/TicketingDaoTest.cs
@@ -6,18 +6,39 @@ namespace ZooTest.Persistence
 {
     public class TicketingDaoTest
     {
+        private TicketingDao Dao { get; set; }
+
+        public TicketingDaoTest()
+        {
+            Dao = new TicketingDao();
+        }
+
         [Fact]
         public void ShouldSetTicketIdAfterSavingTicket()
         {
-            var dao = new TicketingDao();
             var ticket = new Ticket
             {
                 AttendanceDate = "07/01/2019"
             };
 
-            dao.Save(ticket);
+            Dao.Save(ticket);
 
             Assert.NotNull(ticket.Id);
+        }
+
+        [Fact]
+        public void ShouldFindSavedTickets()
+        {
+            var ticket = new Ticket
+            {
+                AttendanceDate = "07/23/2019"
+            };
+
+            Dao.Save(ticket);
+
+            var persistedTicket = Dao.Find(ticket.Id);
+
+            Assert.Equal(ticket, persistedTicket);
         }
     }
 }
