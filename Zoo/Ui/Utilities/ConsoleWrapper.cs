77d9fe704a4086e20973058d30b51a5c1a0ad7f7
riojack <ayjay122@gmail.com>
2019-09-23 15:28:19 -0500

    wrap around .net's Console

---
+++ Zoo/Ui/Utilities/ConsoleWrapper.cs
@@ -0,0 +1,23 @@
+using System;
+using System.Threading.Tasks;
+
+namespace Zoo.Ui.Utilities
+{
+    public class ConsoleWrapper
+    {
+        public virtual async Task WriteLineAsync(string line)
+        {
+            await Console.Out.WriteLineAsync(line);
+        }
+
+        public virtual async Task<string> ReadLineAsync()
+        {
+            return await Console.In.ReadLineAsync();
+        }
+
+        public virtual void SetCursorPosition(int left, int top)
+        {
+            Console.SetCursorPosition(left, top);
+        }
+    }
+}
