03482b0d88e6f9e9b8b2c01f728e0b17eeb41292
riojack <ayjay122@gmail.com>
2019-09-26 15:18:07 -0500

    refactor ConsoleWrapper and inline methods to expression bodies

--- Zoo/Ui/Utilities/ConsoleWrapper.cs
+++ Zoo/Ui/Utilities/ConsoleWrapper.cs
@@ -5,24 +5,12 @@ namespace Zoo.Ui.Utilities
 {
     public class ConsoleWrapper
     {
-        public virtual async Task WriteLineAsync(string line)
-        {
-            await Console.Out.WriteLineAsync(line);
-        }
+        public virtual async Task WriteLineAsync(string line) => await Console.Out.WriteLineAsync(line);
 
-        public virtual async Task<string> ReadLineAsync()
-        {
-            return await Console.In.ReadLineAsync();
-        }
+        public virtual async Task<string> ReadLineAsync() => await Console.In.ReadLineAsync();
 
-        public virtual void SetCursorPosition(int left, int top)
-        {
-            Console.SetCursorPosition(left, top);
-        }
+        public virtual void SetCursorPosition(int left, int top) => Console.SetCursorPosition(left, top);
 
-        public virtual void ClearScreen()
-        {
-            Console.Clear();
-        }
+        public virtual void ClearScreen() => Console.Clear();
     }
 }
